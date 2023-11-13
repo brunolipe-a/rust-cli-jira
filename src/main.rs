e5cb03db4173d740ca8889d5bb672a9d33733fbe
Bruno <bfaa1999@gmail.com>
2023-11-13 02:33:30 -0300

    fix: changing main to call JiraDatabase

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use anyhow::{Error, Ok};
-use db::Database;
+use db::JiraDatabase;
 
 mod db;
 mod models;
 
 fn main() -> Result<(), Error> {
-    let json_db = db::JSONFileDatabase {
-        file_path: "data/db.json".to_owned(),
-    };
+    let json_db = JiraDatabase::new("data/db.json".to_owned());
 
     let db_state = json_db.read_db()?;
 
